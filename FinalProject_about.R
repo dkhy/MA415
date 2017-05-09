5aa96b781be0bfd8dbac8a8b2337db9b406c75a6
dkhy <dkhy@bu.edu>
2017-05-09 01:38:36 -0400

    Add files via upload

---
+++ FinalProject_about.R
@@ -0,0 +1,29 @@
+tabPanel("About",
+         HTML(
+           '<p style="text-align:justify">This course gave me the chance to learn new programming language for better data analysis. I sincerely appreciate Dr. Haviland Wright for sharing his knowledge and programming experience with us.
+           It is too bad that I could not do my best for this course due to my heavy schedule for writing manuscript and visiting conference. However, I strongly believe that I did my best for the final project, even if it may look nothing. 
+           For final project, I have read the class materials from cover to cover, but it does not seem to be enough to recover from a losing points. I will sincerely and seriously appreciate his helps, supports, and generosity for his grading policies. 
+           At the begining of the last semester, I became a father (the name of my princess is Grace, now 8 month infant). Since no one helped my wife, I had to help her during semester. Also, I have submitted the first author research paper at the end of April while I have been taking 15 credits in this semester.
+          I know how I am not good sutdent for this course and for you. I apologize you for those again. However, I am sure that I have learned a lot of new programming skills froma this course, which should be helpful for my future career. It is my last semester for my PhD coursework and I will take PhD comprehensive exam from June 1st. If I get under B, 
+          I will lose my fellopship. If so, I will not be able to support my family.
+            For these reasons (excuses), I need at least B. I will do more for the final project, if you think my current deliverables are not enough. Please let me know I need more than current one. Thanks for your time and considerations.'),
+         
+         HTML('
+              <div style="clear: left;"><img src="http://www.bu.edu/motordevlab/files/2015/10/Daekyoo.png" alt="" style="float: left; margin-right:5px" /></div>
+              <p>Daekyoo Kim<br/>
+              PhD student | R beginner<br/>
+              <a href="http://www.bu.edu/motordevlab/people/" target="_blank">Profile</a> | 
+              <a href="https://github.com/dkhy/MA415" target="_blank">Github</a> 
+              </p>'),
+         
+         fluidRow(
+           column(4,
+                  HTML('
+                       
+                       </ul>
+                       
+                       </ul>')
+                  )
+                  ),
+         value="about"
+                  )
